7324665cd04ce91e539754cdf4b8f559a5baeda2
shaik-sohail-72 <shaiksohailhu7n@gmail.com>
2023-08-17 20:13:28 +0530

    Created Home page component

---
+++ q2_trainfrontend/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+import AllTrains from './AllTrains';
+import Navbar from './Navbar';
+const Home = () => {
+  return (
+    <div>
+    <Navbar />
+    <AllTrains />
+    <Footer />
+    </div>
+  );
+};
+export default Home;
